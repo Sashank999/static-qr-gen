7eaf43d54b4569ac261de475bf625a9b71b0126f
Sashank999 <bandi.rao999@gmail.com>
2024-05-21 09:40:49 +0530

    Add `@angular/pwa` service worker caching.
    Steps from
    https://angular.io/guide/service-worker-getting-started#cli-command
    `src/assets/icons` are generated from
    https://www.pwabuilder.com/imageGenerator
    The input is the QR of https://www.example.com.
    384x384 is generated separately.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideNoopAnimations()]
+  providers: [provideRouter(routes), provideNoopAnimations(), provideServiceWorker('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        registrationStrategy: 'registerWhenStable:30000'
+    })]
 };
